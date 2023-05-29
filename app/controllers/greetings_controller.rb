f72aacc6d4b4f364b3e0e30474af085221e006dd
tarikbouari <tarikbouari@gmail.com>
2023-05-29 11:34:12 +0000

    Add greeting controller

---
+++ app/controllers/greetings_controller.rb
@@ -0,0 +1,9 @@
+class GreetingsController < ApplicationController
+    def index
+        @greetings = Greeting.all
+    end
+    def random_greeting
+        @greeting = Greeting.order("RANDOM()").first
+         render json:  @greeting, status: :ok
+    end
+end
