12050a7ba219973d1b221f954a5175684f6dfc83
tarikbouari <tarikbouari@gmail.com>
2023-05-29 12:37:37 +0000

    Fix linters

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,9 +1,10 @@
 class GreetingsController < ApplicationController
-    def index
-        @greetings = Greeting.all
-    end
-    def random_greeting
-        @greeting = Greeting.order("RANDOM()").first
-         render json:  @greeting, status: :ok
-    end
+  def index
+    @greetings = Greeting.all
+  end
+
+  def random_greeting
+    @greeting = Greeting.order('RANDOM()').first
+    render json: @greeting, status: :ok
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'greetings#index'
+
+  resources :greetings
+  get 'random_greeting', to: 'greetings#random_greeting'
 end
